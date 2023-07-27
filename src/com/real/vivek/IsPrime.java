07c877c93a942572198896b3201a9fd00d285dd8
Vivek Yadav <vivek.mahadev.yadav@gmail.com>
2023-07-27 20:08:42 +0530

    Code optimization for finding if a number is Prime or not

--- src/com/real/vivek/IsPrime.java
+++ src/com/real/vivek/IsPrime.java
@@ -9,11 +9,15 @@ public class IsPrime {
 		System.out.println("Enter a number:");
 		int num = sc.nextInt();
 		boolean isPrime = true;
-		// We iterate from 2 till the number-1 and check if num is divisible by any number
-		// If it is divisible then num is non-prime
-		for (int i = 2; i < num; i++) {
+		// Optimization: If there are any factors of a number they will occur till its square root
+		// i.e. any number(factor) that divides our number completely has to occur till our square root  
+		// Thus we have to iterate will square of the number only if there is any number that divides our given number
+		// Then num is non-prime, we have to check for square root also hence the = sign in condition
+		// The time complexity of the code is now sqRoot(n) 
+		for (int i = 2; i*i <= num; i++) {
 			if (num % i == 0) {
 				isPrime = false;
+				break;
 			}
 		}
 		System.out.println((num == 2 || isPrime) ? "Number " + num + " is Prime" : "Number " + num + " is Non-Prime");
